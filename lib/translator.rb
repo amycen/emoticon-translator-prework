e1ba87c38697e425d08b58f8fc07918417e7328e
Amy Cen <amy.cen@blackrock.com>
2019-04-20 05:42:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def load_library
   # code goes here
-  emoticons = YAML::load(File.open('./lib/emoticons.yml'))
+  emoticons = YAML.load_file('./lib/emoticons.yml')
   emoticons
 end
 
